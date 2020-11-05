2b2be6cfc2d68232615b83d0e464b02ffb5ee3da
Samantha Fok <samantha.fok@hotmail.com>
2020-11-05 02:17:25 +0000

    Made a code to search for word horizontally/vertically through an array

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,19 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalFlip = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        verticalFlip[i] = [];
+        for (let k = 0; k < letters.length; k++) {
+            verticalFlip[i][k] = letters[k][i];
+        }
+    }
+    const verticalJoin = verticalFlip.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false; 
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
